9ba707e7dee2a06e3e6828da301934551fab7966
eliudheredia <edu.heredia@ymail.com>
2022-03-28 15:35:59 -0400

    displaying likes and dislikes tuits

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -5,6 +5,9 @@ const AUTH_API = `${BASE_URL}/api/auth`;
 
 const api = axios.create({
     withCredentials: true,
+    method: ["GET", "POST", "PUT", "DELETE"],
+    headers: ['Content-type', 'application/json'],
+    url: BASE_URL
 });
 
 export const signup = (user) =>
